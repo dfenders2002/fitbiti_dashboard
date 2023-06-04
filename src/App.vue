f740608f2d4f9da70e082cb7b67f4adde67449c0
Daan Enderrs <58844581+dfenders2002@users.noreply.github.com>
2023-06-04 08:48:52 +0200

    routing fix

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import { useStore } from './store/store';
 
 const store = useStore();
 const isLoggedIn = ref(store.state.isLoggedIn);
-const users = ref([
+const dummyUsers = [
   {id: 1, pid: 'PID123456789', age: 23, heightCm: 175, weightKg: 69, diagnose: 'aids', inTreatment: true },
   {id: 2, pid: 'PID987654321', age: 28, heightCm: 175, weightKg: 62, diagnose: 'kanker', inTreatment: false },
   {id: 3, pid: 'PID456789123', age: 41, heightCm: 188, weightKg: 90, diagnose: 'ebola', inTreatment: false },
@@ -21,9 +21,30 @@ const users = ref([
   {id: 5, pid: 'PID111222333', age: 32, heightCm: 170, weightKg: 60, diagnose: 'diabetes', inTreatment: false },
 
   // add more users here...
-]);
+];
+
+const users = ref([]);
+const fetchFitbitUsers = async () => {
+  try {
+    const response = await fetch('https://localhost:7287/fitbit');
+    const data = await response.json();
+    data.forEach((user) => {
+      const birthDate = new Date(user.dateOfBirth); 
+      const now = new Date(); 
+      const ageDiff = now.getTime() - birthDate.getTime(); 
+      const ageDate = new Date(ageDiff); 
+      user.age = Math.abs(ageDate.getUTCFullYear() - 1970); 
+    });
+    users.value = [...dummyUsers, ...data]; 
+  } catch (error) {
+    console.log('Error fetching Fitbit users:', error);
+  }
+};
+
+
 
 onMounted(() => {
+  fetchFitbitUsers();
   const token = localStorage.getItem('token');
   if (token) {
     store.commit('login');
